bca247d1fbe235f49fb53096369479169013fbb6
Georgy Dyagilev <g.dyagilev@gmail.com>
2018-12-22 13:30:01 +0700

    Initial Commit

---
+++ OBD2 Decoder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OBD2 Decoder
+//
+//  Created by Georgy Dyagilev on 22/12/2018.
+//  Copyright © 2018 Georgy Dyagilev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
